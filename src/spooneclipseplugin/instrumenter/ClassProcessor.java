133d466a8fec65c74cfd25321ff12fa5bfeae0a5
THIBAULT BEZIERS LA FOSSE <thiblf@gmail.com>
2017-05-18 17:11:27 +0200

    Added a print stack trace in a catch

--- src/spooneclipseplugin/instrumenter/ClassProcessor.java
+++ src/spooneclipseplugin/instrumenter/ClassProcessor.java
@@ -10,7 +10,6 @@ public class ClassProcessor extends AbstractProcessor<CtType<?>> {
 
 	@Override
 	public void process(CtType<?> clazz) {
-			// test
 			clazz.getMethods().forEach(m -> {
 				String toAdd = "System.out.println(\""+m.getSimpleName()+" executed!\")";
 				CtCodeSnippetStatement ctCodeSnippetStatement = getFactory().Core().createCodeSnippetStatement();
@@ -20,6 +19,7 @@ public class ClassProcessor extends AbstractProcessor<CtType<?>> {
 						m.getBody().insertBegin(ctCodeSnippetStatement);	
 					}
 				} catch (SpoonException e) {
+					e.printStackTrace();
 				}
 			});
 	}
